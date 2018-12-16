fbc1eb914cce917568719daac89b0ea53e2aa0d8
tomaszkoz94 <tomaszkoz94@gmail.com>
2018-12-16 21:57:12 +0100

    Rosenbrock jako dodatkowa funkcja, liczona nie przy każdym tworzeniu obiektu

--- src/AlgorithmDE/Point.java
+++ src/AlgorithmDE/Point.java
@@ -3,22 +3,16 @@ package AlgorithmDE;
 public class Point {
     public double X;
     public double Y;
-    public double RosenBrock;
 
     public Point(double X, double Y){
         this.X = X;
         this.Y = Y;
-        RosenBrock = CountRosenBrock();
-    }
-
-    public double CountRosenBrock() {
-        double temp =  Math.pow(1 - getX(), 2);
-        double temp2 = Math.pow(getY() - (getX() * getX()), 2);
-        return temp + temp2;
     }
 
     public double getRosenBrock() {
-        return RosenBrock;
+        double temp =  Math.pow(1 - getX(), 2);
+        double temp2 = Math.pow(getY() - (getX() * getX()), 2);
+        return temp + temp2;
     }
 
     public double getX() {
